f2d05621f504e3d2cf42e31cf705eec7861a6854
“mut0xE” <210969342+mut0xE@users.noreply.github.com>
2026-05-17 11:35:31 +0530

    test: join players

--- week-3/dungeon-vault/tests/dungeon-vault.ts
+++ week-3/dungeon-vault/tests/dungeon-vault.ts
@@ -30,6 +30,7 @@ describe("dungeon-vault", () => {
   let creator = provider.wallet;
   const player2 = Keypair.generate();
   const player3 = Keypair.generate();
+  const player4 = Keypair.generate();
 
   const programId = program.programId;
   const connection = provider.connection;
@@ -60,6 +61,7 @@ describe("dungeon-vault", () => {
     );
   });
 
+  // 1. Initialize Dungeon
   it("creates dungeon with correct state", async () => {
     await program.methods
       .initializeDungeon(dungeonId, entryFee, maxPlayers)
@@ -107,7 +109,7 @@ describe("dungeon-vault", () => {
   });
 
   it("fails with invalid max players (1 player)", async () => {
-    const id = new BN(100);
+    const id = getDungeonId();
     const [pda] = getDungeonPDA(id, creator.publicKey, programId);
     const [vault] = getVaultPDA(pda, programId);
 
@@ -125,8 +127,135 @@ describe("dungeon-vault", () => {
         .rpc();
       assert.fail("Should have thrown");
     } catch (err: any) {
-      // console.log(err.message);
+      console.log(err.message);
       assert.include(err.message, "NotEnoughPlayers");
+    }
+  });
+
+  // 2. Join Dungeon
+
+  it("player1 joins and vault receives entry fee", async () => {
+    const vaultBefore = await connection.getBalance(vaultPDA);
+    console.log("vaultBefore", vaultBefore / LAMPORTS_PER_SOL);
+
+    await program.methods
+      .joinDungeon(dungeonId)
+      .accounts({
+        player: creator.publicKey,
+        //@ts-ignore
+        dungeon: dungeonPDA,
+        playerState: player1StatePDA,
+        vault: vaultPDA,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([creator.payer])
+      .rpc();
+
+    const dungeon = await program.account.dungeon.fetch(dungeonPDA);
+    const playerState = await program.account.playerState.fetch(
+      player1StatePDA
+    );
+    const vaultAfter = await connection.getBalance(vaultPDA);
+    console.log("vaultAfter", vaultAfter / LAMPORTS_PER_SOL);
+
+    assert.equal(playerState.player.toBase58(), creator.publicKey.toBase58());
+    assert.equal(playerState.alive, true);
+    assert.equal(playerState.currentChoice, 0);
+    assert.isAbove(vaultAfter, vaultBefore);
+    assert.equal(dungeon.amount.toString(), entryFee.toString());
+  });
+
+  it("player2 joins", async () => {
+    const vaultBefore = await connection.getBalance(vaultPDA);
+    console.log("vaultBefore", vaultBefore / LAMPORTS_PER_SOL);
+
+    await program.methods
+      .joinDungeon(dungeonId)
+      .accounts({
+        player: player2.publicKey,
+        //@ts-ignore
+        dungeon: dungeonPDA,
+        playerState: player2StatePDA,
+        vault: vaultPDA,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([player2])
+      .rpc();
+
+    const dungeon = await program.account.dungeon.fetch(dungeonPDA);
+    const playerState = await program.account.playerState.fetch(
+      player2StatePDA
+    );
+    const vaultAfter = await connection.getBalance(vaultPDA);
+    console.log("vaultAfter", vaultAfter / LAMPORTS_PER_SOL);
+
+    assert.equal(playerState.player.toBase58(), player2.publicKey.toBase58());
+    assert.equal(playerState.alive, true);
+    assert.equal(playerState.currentChoice, 0);
+    assert.isAbove(vaultAfter, vaultBefore);
+
+    assert.equal(dungeon.amount.toString(), entryFee.muln(2).toString());
+    assert.equal(dungeon.alivePlayers, 2);
+  });
+
+  it("player3 joins and game becomes Active", async () => {
+    const vaultBefore = await connection.getBalance(vaultPDA);
+    console.log("vaultBefore", vaultBefore / LAMPORTS_PER_SOL);
+
+    await program.methods
+      .joinDungeon(dungeonId)
+      .accounts({
+        player: player3.publicKey,
+        //@ts-ignore
+        dungeon: dungeonPDA,
+        playerState: player3StatePDA,
+        vault: vaultPDA,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([player3])
+      .rpc();
+
+    const dungeon = await program.account.dungeon.fetch(dungeonPDA);
+    const playerState = await program.account.playerState.fetch(
+      player3StatePDA
+    );
+    const vaultAfter = await connection.getBalance(vaultPDA);
+    console.log("vaultAfter", vaultAfter / LAMPORTS_PER_SOL);
+
+    assert.equal(playerState.player.toBase58(), player3.publicKey.toBase58());
+    assert.equal(playerState.alive, true);
+    assert.equal(playerState.currentChoice, 0);
+    assert.isAbove(vaultAfter, vaultBefore);
+
+    assert.equal(dungeon.amount.toString(), entryFee.muln(3).toString());
+    assert.equal(dungeon.alivePlayers, 3);
+    assert.deepEqual(dungeon.status, { active: {} }); // game started
+  });
+
+  it("fails when dungeon is full (already Active)", async () => {
+    await airdrop(provider, creator.payer, player4.publicKey);
+    const [p4StatePDA] = getPlayerStatePDA(
+      dungeonPDA,
+      player4.publicKey,
+      programId
+    );
+
+    try {
+      await program.methods
+        .joinDungeon(dungeonId)
+        .accounts({
+          player: player4.publicKey,
+          //@ts-ignore
+          dungeon: dungeonPDA,
+          playerState: p4StatePDA,
+          vault: vaultPDA,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([player4])
+        .rpc();
+      assert.fail("Should have thrown");
+    } catch (err: any) {
+      assert.include(err.message, "GameAlreadyStarted");
     }
   });
 });
